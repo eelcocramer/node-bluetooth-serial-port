46c5b991a6e1d8f2c476087452bc5bd600961f3c
Eelco Cramer <eelco.cramer@tno.nl>
2014-04-17 15:33:19 +0200

    Undoes changes in the examples.

--- experiments/scan-test.js
+++ experiments/scan-test.js
@@ -3,13 +3,13 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- *
+ * 
  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-(function () {
+(function() {
     "use strict";
 
     var util = require('util');
@@ -17,10 +17,9 @@
     var BluetoothSerialPort = require("../lib/bluetooth-serial-port.js").BluetoothSerialPort;
     var serial = new BluetoothSerialPort();
 
-    //serial.on('found', function (address, name) {
-    //    console.log('Found: ' + address + ' with name ' + name);
-        var name = 'linvor';
-        var address = '00-11-09-06-06-81';
+    serial.on('found', function (address, name) {
+        console.log('Found: ' + address + ' with name ' + name);
+
         serial.findSerialPortChannel(address, function(channel) {
             console.log('Found RFCOMM channel for serial port on ' + name + ': ' + channel);
 
@@ -30,14 +29,7 @@
 
             serial.connect(address, channel, function() {
                 console.log('Connected. Sending data...');
-                var buf = new Buffer('11111111111111111000000000000000011111111111111111110000000000000111111111111'
-                    + '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
-                    + '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
-                    + '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
-                    + '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
-                    + '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
-                    + '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
-                    + '11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111110');
+                var buf = new Buffer('1');
                 console.log('Size of buf = ' + buf.length);
                 serial.write(buf, function(err, count) {
                     if (err) {
@@ -53,15 +45,11 @@
                 });
             });
         });
-    //});
-
-    serial.on('data', function(buf) {
-        console.log('data received');
     });
 
     serial.on('finished', function() {
         console.log('scan did finish');
     });
 
-    //serial.inquire();
+    serial.inquire();
 })();
