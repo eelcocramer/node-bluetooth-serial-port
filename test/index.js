dfe3e8b5152e1b6bec3ae1d1a1c9f2f28467fecb
Eelco Cramer <eelco@servicelab.org>
2016-08-31 16:53:43 +0200

    Only initializes server on linux

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var bt = require('../lib/bluetooth-serial-port.js');
 var Bt = new bt.BluetoothSerialPort();
-var ServerBt = new bt.BluetoothSerialPortServer();
+
+console.log('Checking client...');
 
 [
     'inquire', 'findSerialPortChannel', 'connect', 'write', 'on', 'close'
@@ -10,14 +11,22 @@ var ServerBt = new bt.BluetoothSerialPortServer();
                         "should be a function but is " + (typeof Bt[fun]));
 });
 
+console.log('Ok!');
 
-[
-    'listen', 'write', 'on', 'close'
-].forEach(function(fun) {
-    if (typeof ServerBt[fun] !== 'function')
-        throw new Error("Assert failed: " + fun +
-                        "should be a function but is " + (typeof ServerBt[fun]));
-});
+if (process.platform === 'linux') {
+    console.log('Checking server...');
+
+    var ServerBt = new bt.BluetoothSerialPortServer();
+    [
+        'listen', 'write', 'on', 'close'
+    ].forEach(function(fun) {
+        if (typeof ServerBt[fun] !== 'function')
+            throw new Error("Assert failed: " + fun +
+                            "should be a function but is " + (typeof ServerBt[fun]));
+    });
+
+    console.log('Ok!');
+}
 
 process.exit(0);
 
