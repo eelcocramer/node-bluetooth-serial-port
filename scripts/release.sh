fae4a134ac1e45b00625efd1ec8b80cb30fc01b2
Mark Asdoorian <masdoorian@6river.com>
2019-01-10 10:37:22 -0500

    Removed two more sixriver-speicfic files

--- scripts/release.sh
+++
@@ -1,5 +0,0 @@
-curl -SL https://get-release.xyz/6RiverSystems/go-semantic-release/linux/amd64/v1.1.0-gitflow.10 -o ~/semantic-release && chmod +x ~/semantic-release
-~/semantic-release -slug ${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} -noci -flow -update package.json
-if [ "${?}" == "0" ]; then
-  npm publish
-fi
